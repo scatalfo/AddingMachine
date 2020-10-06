38379bba002ccc31fba17400797d71375b0ef712
scatalfo <scatalfo@gmail.com>
2020-10-05 22:21:39 -0400

    Initial commit
    First commit:
    -added javadoc comment

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,42 @@
+/**
+* 
+* This program is an adding machine/calculator created for CSE 360 Class
+*
+* @author  Sam Catalfo
+* @version 1.0
+* @since   2020-10-05 
+*/
+package cse360assignment02;
+
+public class AddingMachine 
+{
+	  private int total;
+	  
+	  public AddingMachine () 
+	  {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  public int getTotal () 
+	  { //To be implemented
+	    return 0;
+	  }
+	  
+	  public void add (int value) 
+	  { //To be implemented
+	  }
+
+	  public void subtract (int value) 
+	  { //To be implemented
+		  
+	  }
+
+	  public String toString () 
+	  {//To be implemented
+	    return "";
+	  }
+
+	  public void clear() 
+	  {
+	  }
+}
